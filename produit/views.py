0cdf90808f2ec9e81e5f2e9702da4d3fdf09b1b9
soulemaneyeo99 <157871114+soulemaneyeo99@users.noreply.github.com>
2024-03-31 14:42:40 +0000

    Add files via upload

---
+++ produit/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from commande.models import Commande
+from client.models import Client
+from django.contrib.auth.decorators import login_required
+
+
+@login_required(login_url='accès')
+def home(request):
+    commandes = Commande.objects.all()
+    clients = Client.objects.all()
+    context = {'commandes': commandes, 'clients': clients}
+    return render(request, 'produit/accueil.html', context)
